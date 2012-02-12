6f44682e04faa58f46a35fe5c0b9e43c7bbb0b95
Sam Grossberg <sam@gopollgo.com>
2012-02-12 12:23:10 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Boilerplate::Application.config.secret_token = 'a87f9f17299b3debcd32b0a6808b4d9f7240558a6b8876250872fd6e2e9409441d6c314cc7b3e95eac37212df64b41c61d0fc69aac619885f335e542581509bf'
