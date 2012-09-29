2d0f6375512af2180887b1c8e2dc90cebb6df18c
Sam Grossberg <sam.grossberg@gmail.com>
2012-09-29 14:51:47 -0700

    Remember login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  private
+    def current_user
+      @current_user ||= User.find(session[:user_id])
+    end
+    helper_method :current_user
+
+    def logged_in?
+      current_user.present?
+    end
+    helper_method :logged_in?
+
 end
